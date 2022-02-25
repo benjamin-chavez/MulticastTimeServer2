f76ae7ad2cdd9ad00f2258e7d7601e3501ece574
Ben Chavez <ben.m.chavez@gmail.com>
2022-02-25 04:54:01 -0600

    init commit

---
+++ MulticastClient.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.*;
+
+public class MulticastClient {
+
+  public static void main(String[] args) {
+    System.out.println("Multicast Time Client");
+    try (MulticastSocket socket = new MulticastSocket(8888)) {
+      InetAddress mcastaddr = InetAddress.getByName("224.0.0.0");
+      InetSocketAddress group = new InetSocketAddress(mcastaddr, port);   //<= Not sure what the port her is actually doing..
+      NetworkInterface netIf = NetworkInterface.getByName("bge0");
+      socket.joinGroup(group, netIf);
+      System.out.println("Multicast Group Joined)");
+
+      byte[] buffer = new byte[256];
+      DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+
+      for (int i = 0; i < 100; i++) {
+        socket.receive(packet);
+        String received = new String(packet.getData());
+        System.out.println(received.trim());
+      }
+      socket.leaveGroup(group, netIf);
+    } catch (IOException ex) {
+      //TODO: handle exception
+    }
+    System.out.println("Multicast Time Client Terminated");
+  }
+}
---
+++ MulticastServer.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+public class MulticastServer {
+
+  public static void main(String[] args) {
+    System.out.println("Multicast Time Server");
+    DatagramSocket serverSocket = null;
+    try {
+      serverSocket = new DatagramSocket();
+
+      while(true) {
+        String dateText = new Date().toString();
+        byte[] buffer = new byte[256];
+        buffer = dateText.getBytes();
+
+        InetAddress group = InetAddress.getByName("224.0.0.0");
+        DatagramPacket packet;
+        packet = new DatagramPacket(buffer, buffer.length, group, 8888);
+        serverSocket.send(packet);
+        System.out.println("Time sent: " + dateText);
+
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+          //TODO: handle exception
+        }
+      }
+
+    } catch (SocketException ex) {
+      //TODO: handle exception
+    } catch (IOException ex) {
+      //TODO: handle exception
+    }
+  }
+}
